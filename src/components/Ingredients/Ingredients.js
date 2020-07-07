41971895fb2398e61e5b9f39259f3d7a44f92269
Chinmay <Chinmay8116@gmail.com>
2020-07-07 17:28:07 +0530

    added useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,46 @@
-import React,{ useState } from 'react';
+import React,{ useState,useEffect } from 'react';
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients=()=> {
+  const [useringredients,SetIngredients]=useState(new Array());
+useEffect(()=>{
+    fetch('https://reacthooks-eb57d.firebaseio.com/ingredients.json')
+    .then(response=>{
+      return response.json()
+    })
+    .then(responseData=>{
+      const loadedIngredients=[];
+      for(let ing in responseData) {
+        loadedIngredients.push({
+          id:ing,
+          title:responseData[ing].title,
+          amount:responseData[ing].amount
+        });
+      }
+      SetIngredients(loadedIngredients);
+    });
+},new Array())
 
-const [useringredients,SetIngredients]=useState(new Array());
+
 
 const addIngredient=(ingredient)=>{
-  SetIngredients(prevIngredient=>[
-    ...prevIngredient,
-    {
-      id: Math.random.toString,...ingredient
-    }
-  ]);
+  fetch('https://reacthooks-eb57d.firebaseio.com/ingredients.json',{
+    method:'POST',
+    body:JSON.stringify(ingredient),
+    headers:{'Content-Type':'application/json'}
+  })
+  .then(response=>{
+      return response.json()
+  })
+  .then(responseData=>{
+        SetIngredients(prevIngredient=>[
+          ...prevIngredient,
+          { id: responseData.name,...ingredient }
+        ]);
+  });
+  
 }
 const removeIngredient=(ingredientId)=>{
   SetIngredients(prevIngredient=>prevIngredient.filter((ingredient)=>ingredient.id !==ingredientId));
