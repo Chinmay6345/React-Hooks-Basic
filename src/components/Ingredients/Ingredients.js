89d71e352a60c6eb4dd229c54735edbed09fb6d6
Chinmay <Chinmay8116@gmail.com>
2020-07-07 20:15:03 +0530

    added error module

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,11 +1,13 @@
 import React,{ useState,useEffect,useCallback } from 'react';
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 const Ingredients=()=> {
 const [useringredients,SetIngredients]=useState(new Array());
 const[isLoading,SetLoading]=useState(false);
+const[error,SetError]=useState();
 const addIngredient=(ingredient)=>{
   SetLoading(true);
   fetch('https://reacthooks-eb57d.firebaseio.com/ingredients.json',{
@@ -22,16 +24,21 @@ const addIngredient=(ingredient)=>{
           ...prevIngredient,
           { id: responseData.name,...ingredient }
         ]);
-  });
+  }).catch(error=>{
+    SetError(error.message);
+  });;
 }
 const removeIngredient=(ingredientId)=>{
   SetLoading(true);
-  fetch(`https://reacthooks-eb57d.firebaseio.com/ingredients/${ingredientId}.json`,{
+  fetch(`https://reacthooks-eb57d.firebaseio.com/ingredients/${ingredientId}.jso`,{
     method:'DELETE'
   })
   .then(response=>{
     SetLoading(false);
         SetIngredients(ing=>ing.filter((ingredient=>ingredient.id !==ingredientId)));
+  })
+  .catch(error=>{
+    SetError(error.message);
   });
 }
 
@@ -39,8 +46,16 @@ const searchIngredients=useCallback((ingredient)=>{
   SetIngredients(ingredient);
 },new Array());
 
+const clearError=()=>{
+  SetError(null);
+  SetLoading(false);
+}
+
   return (
     <div className="App">
+      {
+        error && <ErrorModal onClose={clearError}>error</ErrorModal>
+      }
       <IngredientForm onAddIngredient={addIngredient} loading={isLoading} />
 
       <section>
